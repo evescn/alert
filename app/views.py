5aa47ddffa493fd6dcc905e7fc3a7f070da840d2
evescn <you@example.com>
2023-06-14 18:55:59 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,6 +23,29 @@ def alert_service(msg):
 
     return response.status_code
 
+def extract_data(title, content):
+    alert_type = title.split('[')[1].split(']')[0].strip()
+    alert_project = title.split('[')[2].split(']')[0].split(':')[1].strip()
+    monitor_item = title.split('[')[3].split(']')[0].split(':')[1].strip()
+    alert_content = content.split('[')[3].split(']')[0].strip() + ', ' + \
+                    (': '.join(content.split('[')[2].split(']')[0].split(':')[:])).strip()
+    alert_time = (':'.join(content.split('[')[4].split(']')[0].split(':')[1:])).strip()
+    alert_interval = content.split('[')[6].split(']')[1]
+
+    return alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval
+
+
+def format_alert_message(alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval):
+    msg = '''
+        # Cat监控告警信息
+        > <font color="warning">告警类型</font>：%s
+        > <font color="warning">告警项目</font>：%s
+        > <font color="warning">监控项目</font>：%s
+        > <font color="warning">告警内容</font>：%s
+        > <font color="warning">告警时间</font>：%s
+        > <font color="warning">告警间隔时间</font>：%s
+    ''' % (alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval)
+    return msg
 
 # Create your views here.
 class Alert(APIView):
@@ -36,34 +59,62 @@ class Alert(APIView):
             content = request.data['content']
 
             # 抽离数据
-            alert_type = title.split('[')[1].split(']')[0].strip()
-            alert_project = title.split('[')[2].split(']')[0].split(':')[1].strip()
-            monitor_item = title.split('[')[3].split(']')[0].split(':')[1].strip()
-            alert_content = content.split('[')[3].split(']')[0].strip() + ', ' + \
-                            (': '.join(content.split('[')[2].split(']')[0].split(':')[:])).strip()
-            alert_time = (':'.join(content.split('[')[4].split(']')[0].split(':')[1:])).strip()
-            alert_interval = content.split('[')[6].split(']')[1]
+            alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval = extract_data(
+                title,
+                content
+            )
 
             # 组装数据
-            msg = '''
-                # Cat监控告警信息
-                > <font color="warning">告警类型</font>：%s
-                > <font color="warning">告警项目</font>：%s
-                > <font color="warning">监控项目</font>：%s
-                > <font color="warning">告警内容</font>：%s
-                > <font color="warning">告警时间</font>：%s
-                > <font color="warning">告警间隔时间</font>：%s
-            ''' % (alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval)
-            logger.info(str(msg))
+            msg = format_alert_message(
+                alert_type,
+                alert_project,
+                monitor_item,
+                alert_content,
+                alert_time,
+                alert_interval
+            )
+
             status = alert_service(msg)
-            return Response(status)
+            logger.info({'status': status})
+            return Response({'status': status})
         except Exception as e:
             logger.error("发生了异常: %s", str(e))
-            return Response(500)
+            logger.info({'status': '500'})
+            return Response({'status': '500'})
 
 
 class Mail(APIView):
 
     def post(self, request):
-        print(request.data)
-        return Response({'status': '200'})
+        logger.info(request.data)
+
+        try:
+            # 提取数据
+            tmp_data = request.data['value']
+            print(tmp_data)
+            # title = request.data['value']
+            # content = request.data['content']
+            #
+            # # 抽离数据
+            # alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval = extract_data(
+            #     title,
+            #     content
+            # )
+            #
+            # # 组装数据
+            # msg = format_alert_message(
+            #     alert_type,
+            #     alert_project,
+            #     monitor_item,
+            #     alert_content,
+            #     alert_time,
+            #     alert_interval
+            # )
+            #
+            # status = alert_service(msg)
+            # logger.info({'status': status})
+            # return Response({'status': status})
+        except Exception as e:
+            logger.error("发生了异常: %s", str(e))
+            logger.info({'status': '500'})
+            return Response({'status': '500'})
