df466a20879829306069df3db43e071b7e36466e
evescn <you@example.com>
2023-06-15 17:36:46 +0800

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -7,10 +7,12 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import logging
 import re
+import os
 
 # 获取日志器对象
 logger = logging.getLogger('app')
-
+# 获取环境变量
+debug = os.getenv('DEBUG')
 
 def extract_data(title, content, type=''):
     alert_type = title.split('[')[1].split(']')[0].strip()
@@ -27,7 +29,7 @@ def extract_data(title, content, type=''):
         match = re.search(pattern, content)
         if match:
             http_address = match.group(1)
-            if config.DEBUG:
+            if debug.lower() == 'true' or config.DEBUG:
                 alert_url = http_address.replace("cat-web-server", config.test_url_address)
             else:
                 alert_url = http_address.replace("cat-web-server", config.prod_url_address)
@@ -72,7 +74,7 @@ def alert_service(msg):
         }
     }
     # 请求企业微信机器人接口
-    if config.DEBUG:
+    if debug.lower() == 'true' or config.DEBUG:
         response = requests.post(config.test_webhook, data=json.dumps(payload))
     else:
         response = requests.post(config.prod_webhook, data=json.dumps(payload))
