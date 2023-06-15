4608710b418baa26321f6ce75a57a837a4ecdf71
evescn <you@example.com>
2023-06-15 16:25:05 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -50,39 +50,39 @@ class Mail(APIView):
 
     def post(self, request):
         logger.info(request.data)
-        try:
-            # 提取数据
-            tmp_data = request.data['value']
-            title = tmp_data.split(',')[0].strip()
-            content = tmp_data.split(',')[1].strip()
+        # try:
+        # 提取数据
+        tmp_data = request.data['value']
+        title = tmp_data.split(',')[0].strip()
+        content = tmp_data.split(',')[1].strip()
 
-            # 抽离数据
-            alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval, alert_url = utils.extract_data(
-                title,
-                content,
-                'Mail'
-            )
+        # 抽离数据
+        alert_type, alert_project, monitor_item, alert_content, alert_time, alert_interval, alert_url = utils.extract_data(
+            title,
+            content,
+            'Mail'
+        )
 
-            # 组装数据
-            msg = utils.format_alert_message(
-                alert_type,
-                alert_project,
-                monitor_item,
-                alert_content,
-                alert_time,
-                alert_interval,
-                alert_url
-            )
-            # logger.info(msg)
+        # 组装数据
+        msg = utils.format_alert_message(
+            alert_type,
+            alert_project,
+            monitor_item,
+            alert_content,
+            alert_time,
+            alert_interval,
+            alert_url
+        )
+        # logger.info(msg)
 
-            sender = request.data['re']
-            recipient = request.data['to']
-            subject = 'Cat监控告警信息'
-            body = str(msg)
+        sender = request.data['re']
+        recipient = request.data['to']
+        subject = 'Cat监控告警信息'
+        body = str(msg)
 
-            status = utils.send_email(sender, recipient, subject, body)
-            return Response({'status': status})
-        except Exception as e:
-            logger.error("发生了异常: %s", str(e))
-            logger.info({'status': '500'})
-            return Response({'status': '500'})
+        status = utils.send_email(sender, recipient, subject, body)
+        return Response({'status': status})
+        # except Exception as e:
+        #     logger.error("发生了异常: %s", str(e))
+        #     logger.info({'status': '500'})
+        #     return Response({'status': '500'})
